3887f35ee6bfa3cc7a5255c1a2dbf52d44d1ff6d
João Lopes <joaomarcoslopes@id.uff.br>
2023-05-27 11:46:31 -0300

    chore: configures test logger plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,37 @@
+import com.adarshr.gradle.testlogger.theme.ThemeType
+
 plugins {
-    kotlin("jvm") version "1.8.20"
-    application
+	kotlin("jvm") version "1.8.20"
+	id("com.adarshr.test-logger") version "3.2.0"
+	application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
-    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
-    testImplementation("io.kotest:kotest-property:5.6.2")
+	testImplementation(kotlin("test"))
+	testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+	testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+	testImplementation("io.kotest:kotest-property:5.6.2")
+}
+
+testlogger {
+	theme = ThemeType.MOCHA_PARALLEL
 }
 
 tasks.test {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
 
 kotlin {
-    jvmToolchain(11)
+	jvmToolchain(11)
 }
 
 application {
-    mainClass.set("MainKt")
+	mainClass.set("MainKt")
 }
