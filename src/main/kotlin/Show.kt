b49b8de546ee877004f1eb502c053c04cb47cd49
João Lopes <joaomarcoslopes@id.uff.br>
2023-05-28 09:22:33 -0300

    refactor: migrate builder interface to abstract class

--- src/main/kotlin/Show.kt
+++ src/main/kotlin/Show.kt
@@ -4,58 +4,50 @@ import ShowDsl.Rate
 import java.time.LocalDate
 
 data class Show(
-    val name: String,
-    val start: LocalDate,
-    val end: LocalDate? = null,
-    val rating: Rating? = null
+	val name: String,
+	val start: LocalDate,
+	val end: LocalDate? = null,
+	val rating: Rating? = null
 ) {
-    constructor(b: ShowBuilder) : this(
-        b.name ?: throw RequiredField of "name",
-        b.start ?: throw RequiredField of "start",
-        b.end,
-        b.rating
-    )
+	constructor(b: ShowBuilder) : this(
+		b.title ?: throw RequiredField of "name",
+		b.start ?: throw RequiredField of "start",
+		b.end,
+		b.rating
+	)
 
-    interface ShowBuilder : Builder<Show> {
-        var name: String?
-        var start: LocalDate?
-        var end: LocalDate?
-        var rating: Rating?
-        override fun build() = Show(this)
-    }
+	abstract class ShowBuilder : Builder<Show> {
+		var title: String? = null
+		var start: LocalDate? = null
+		var end: LocalDate? = null
+		var rating: Rating? = null
+		override fun build() = Show(this)
+	}
 
-    val favorite: Boolean = rating?.isFavorite ?: false
-    val hasEnded = end != null
+	val favorite: Boolean = rating?.isFavorite ?: false
+	val hasEnded = end != null
 }
 
 interface ShowDsl {
-    val started: LocalDatePicker
-    val ended: LocalDatePicker
+	val started: LocalDatePicker
+	val ended: LocalDatePicker
 
-    val name: Name
+	val name: Name
 
-    fun interface Name {
-        infix fun of(name: String)
-    }
+	fun interface Name {
+		infix fun of(name: String)
+	}
 
-    val rate: Rate
+	val rate: Rate
 
-    fun interface Rate {
-        infix fun it(emoji: StarEmoji)
-    }
+	fun interface Rate {
+		infix fun it(emoji: StarEmoji)
+	}
 }
 
-class ShowDslBuilder : ShowDsl, Builder<Show> {
-    private val builder: ShowBuilder = object : ShowBuilder {
-        override var name: String? = null
-        override var start: LocalDate? = null
-        override var end: LocalDate? = null
-        override var rating: Rating? = null
-    }
-
-    override fun build() = builder.build()
-    override val started = LocalDatePicker { builder.start = it }
-    override val ended = LocalDatePicker { builder.end = it }
-    override val name = Name { builder.name = it }
-    override val rate = Rate { builder.rating = Rating[it] }
+class ShowDslBuilder : ShowDsl, ShowBuilder() {
+	override val started = LocalDatePicker { start = it }
+	override val ended = LocalDatePicker { end = it }
+	override val name = Name { title = it }
+	override val rate = Rate { rating = Rating[it] }
 }
--- src/test/kotlin/ShowTest.kt
+++ src/test/kotlin/ShowTest.kt
@@ -7,49 +7,45 @@ import org.junit.jupiter.api.assertThrows
 import java.time.LocalDate
 
 class ShowTest : StringSpec({
-    "Should throw when builder provides null value for required fields" {
-        forAll(
-            row(object : ShowBuilder {
-                override var name: String? = "Lucifer"
-                override var start: LocalDate? = null
-                override var end: LocalDate? = null
-                override var rating: Rating? = null
-            }),
-            row(object : ShowBuilder {
-                override var name: String? = null
-                override var start: LocalDate? = LocalDate.MIN
-                override var end: LocalDate? = null
-                override var rating: Rating? = null
-            }),
-            row(object : ShowBuilder {
-                override var name: String? = null
-                override var start: LocalDate? = null
-                override var end: LocalDate? = null
-                override var rating: Rating? = null
-            })
-        ) { assertThrows<RequiredField> { Show(it) } }
-    }
+	"Should throw when builder provides null value for required fields" {
+		forAll(
+			row(object : ShowBuilder() {
+				init {
+					title = "Lucifer"
+				}
+			}),
+			row(object : ShowBuilder() {
+				init {
+					start = LocalDate.MIN
+				}
+			}),
+			row(object : ShowBuilder() {})
+		) { assertThrows<RequiredField> { Show(it) } }
+	}
 
-    "Should not throw when builder provides values for required fields" {
-        forAll(
-            row(object : ShowBuilder {
-                override var name: String? = "House"
-                override var start: LocalDate? = LocalDate.MIN
-                override var end: LocalDate? = null
-                override var rating: Rating? = null
-            }),
-            row(object : ShowBuilder {
-                override var name: String? = "House"
-                override var start: LocalDate? = LocalDate.MIN
-                override var end: LocalDate? = null
-                override var rating: Rating? = Rating[3]
-            }),
-            row(object : ShowBuilder {
-                override var name: String? = "House"
-                override var start: LocalDate? = LocalDate.MIN
-                override var end: LocalDate? = LocalDate.MAX
-                override var rating: Rating? = Rating[3]
-            })
-        ) { assertDoesNotThrow { Show(it) } }
-    }
+	"Should not throw when builder provides values for required fields" {
+		forAll(
+			row(object : ShowBuilder() {
+				init {
+					title = "House"
+					start = LocalDate.MIN
+				}
+			}),
+			row(object : ShowBuilder() {
+				init {
+					title = "House"
+					start = LocalDate.MIN
+					rating = Rating[3]
+				}
+			}),
+			row(object : ShowBuilder() {
+				init {
+					title = "House"
+					start = LocalDate.MIN
+					end = LocalDate.MAX
+					rating = Rating[3]
+				}
+			})
+		) { assertDoesNotThrow { Show(it) } }
+	}
 })
